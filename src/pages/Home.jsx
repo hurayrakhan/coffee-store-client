c59a0e282471eb9f85af51949d6c575e01934d70
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-05-18 07:44:26 +0600

    modified popular products section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Link, useLoaderData } from 'react-router';
 import Coffees from '../Components/Coffees';
+import { FaCoffee } from "react-icons/fa";
+
 
 const Home = () => {
     const coffeeData = useLoaderData();
     return (
         <div className='bg-[url("/1.png")] bg-cover bg-no-repeat'>
+            <div>
+                <div className='text-center place-items-center mt-16'>
+                    <p>--- Sip & Savor ---</p>
+                    <h3 className='text-[#331A15] text-2xl font-bold pb-4'>Our Popular Products</h3>
+                    <Link to={'/addCoffee'} className='flex items-center text-white btn bg-[#D2B48C] border-2 border-[#331A15] hover:bg-[#F4F3F0] hover:text-[#331A15]'>Add Coffee <FaCoffee  className='text-[#331A15] text-2xl'/></Link>
+                </div>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-9/12 mx-auto my-10'>
 
                 {
